f625fafe9c4059cbcdd6af0a49aa6d3b54b057f6
dingzishidtc <443155612@qq.com>
2020-04-06 01:14:40 +0800

    2020-4-6

--- Chapter5/src/main/java/com/dingzhi/testng/IgnoreTest.java
+++ Chapter5/src/main/java/com/dingzhi/testng/IgnoreTest.java
@@ -7,10 +7,12 @@ public class IgnoreTest {
     public void ignore1(){
         System.out.println("ignore1 执行");
     }
+
     @Test(enabled = false)
     public void ignore2(){
         System.out.println("ignore2 执行");
     }
+
     @Test(enabled = true)
     public void ignore3(){
         System.out.println("ignore3 执行");
--- Chapter5/src/main/java/com/dingzhi/testng/groups/GroupOnMethod.java
+++ Chapter5/src/main/java/com/dingzhi/testng/groups/GroupOnMethod.java
@@ -6,35 +6,26 @@ import org.testng.annotations.Test;
 
 public class GroupOnMethod {
     @Test(groups = "server")
-    public void test1(){
-        System.out.println("服务端组测试方法1");
-    }
+    public void test1(){ System.out.println("服务端组测试方法1"); }
+
     @Test(groups = "server")
-    public void test2(){
-        System.out.println("服务端组测试方法2");
-    }
+    public void test2(){ System.out.println("服务端组测试方法2"); }
+
     @Test(groups = "client")
-    public void test3(){
-        System.out.println("客户组测试方法3");
-    }
+    public void test3(){ System.out.println("客户组测试方法3"); }
+
     @Test(groups = "client")
-    public void test4(){
-        System.out.println("客户组测试方法4");
-    }
+    public void test4(){ System.out.println("客户组测试方法4"); }
+
     @BeforeGroups("server")
-    public void beforeGroupsServer(){
-        System.out.println("服务端运行前的方法");
-    }
+    public void beforeGroupsServer(){ System.out.println("服务端运行前的方法"); }
+
     @AfterGroups("server")
-    public void afterGroupsServer(){
-        System.out.println("服务端运行后的方法!!!");
-    }
+    public void afterGroupsServer(){ System.out.println("服务端运行后的方法!!!"); }
+
     @BeforeGroups("client")
-    public void beforeGroupsClients(){
-        System.out.println("客户端运行前的方法");
-    }
+    public void beforeGroupsClients(){ System.out.println("客户端运行前的方法"); }
+
     @AfterGroups("client")
-    public void afterGroupsClient(){
-        System.out.println("客户端运行后的方法!!!");
-    }
+    public void afterGroupsClient(){ System.out.println("客户端运行后的方法!!!"); }
 }
